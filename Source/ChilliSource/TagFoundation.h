b88c31ac581ef45d3c48ee0af06fe9bf204dfc5e
SDownie <scott.downie@tag-games.com>
2014-02-25 12:11:47 +0000

    Changing u8 from unsigned char to stdint type

--- Source/ChilliSource/TagFoundation.h
+++ Source/ChilliSource/TagFoundation.h
@@ -58,7 +58,7 @@ typedef int_least16_t		s16;
 typedef int_least32_t		s32;
 typedef int_least64_t		s64;
 
-typedef unsigned char		u8;
+typedef uint_least8_t		u8;
 typedef uint_least16_t		u16;
 typedef uint_least32_t		u32;
 typedef uint_least64_t		u64;
