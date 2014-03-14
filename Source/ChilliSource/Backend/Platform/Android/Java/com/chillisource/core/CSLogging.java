4a95e2b392b39d3703f8e817db32a0f6bf56fb8f
SDownie <scott.downie@tag-games.com>
2014-03-14 10:22:34 +0000

    Adding android logging class

---
+++ Source/ChilliSource/Backend/Platform/Android/Java/com/chillisource/core/CSLogging.java
@@ -0,0 +1,68 @@
+/**
+ *  CSLogging.java
+ *  ChilliSource
+ *
+ *  Created by Scott Downie on 14/03/2014.
+ *  Copyright 2014 Tag Games. All rights reserved.
+ *
+ */
+
+package com.chillisource.core;
+
+import android.util.Log;
+
+/**
+ * Wrapper class around the Android logging system in order to abstract the 
+ * tag.
+ * 
+ * @author S Downie
+ */
+public class CSLogging 
+{
+	/**
+	 * Log message to LogCat under the "verbose" channel
+	 * 
+	 * @author S Downie
+	 * 
+	 * @param Message to display in LogCat
+	 */
+	public static void logDebug(String in_message)
+	{
+		Log.d("ChilliSource", in_message);
+	}
+	/**
+	 * Log message to LogCat under the "warning" channel
+	 * 
+	 * @author S Downie
+	 * 
+	 * @param Message to display in LogCat
+	 */
+	public static void logWarning(String in_message)
+	{
+		Log.w("ChilliSource", in_message);
+	}
+	/**
+	 * Log message to LogCat under the "error" channel
+	 * 
+	 * @author S Downie
+	 * 
+	 * @param Message to display in LogCat
+	 */
+	public static void logError(String in_message)
+	{
+		Log.e("ChilliSource", in_message);
+	}
+	/**
+	 * Log message to LogCat under the "error" channel
+	 * will also cause the application to assert
+	 * 
+	 * @author S Downie
+	 * 
+	 * @param Message to display in LogCat
+	 */
+	public static void logFatal(String in_message)
+	{
+		Log.e("ChilliSource", in_message);
+		assert false;
+	}
+}
