b1ac17a9339ac789e2a0db52fddd5c4b3d95d0a0
I Copland <ian.copland@tag-games.com>
2014-03-27 13:23:42 +0000

    Added Store Deep Linking to android web view.

--- Source/ChilliSource/Backend/Platform/Android/Java/com/chillisource/web/CSWebViewClient.java
+++ Source/ChilliSource/Backend/Platform/Android/Java/com/chillisource/web/CSWebViewClient.java
@@ -1,19 +1,63 @@
 package com.chillisource.web;
 
+import com.chillisource.core.CSApplication;
 import com.chillisource.web.WebViewNativeInterface;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.webkit.WebView;
 
 public class CSWebViewClient extends android.webkit.WebViewClient
 {
+	final String kstrGooglePlayURLScheme = "market";
+	final String kstrAmazonUrlScheme = "amzn";
 	private String mAnchor;
 	private WebView mWebView;
-	
-	@Override 
-	//show the web page in webview but not in web browser
-	public boolean shouldOverrideUrlLoading(WebView view, String url)
-	{ 
-	    view.loadUrl(url); 
+	//--------------------------------------------------------
+	/// Is Store Deep Link
+	///
+	/// @param The URL string.
+	/// @return Whether or not it is a store deep link.
+	//--------------------------------------------------------
+	public boolean IsStoreDeepLink(final String instrUrl)
+	{
+		Uri uri = Uri.parse(instrUrl);
+		if (uri != null)
+		{
+			String strScheme = uri.getScheme();
+			if (strScheme != null)
+			{
+				return (strScheme.equalsIgnoreCase(kstrGooglePlayURLScheme) == true || strScheme.equalsIgnoreCase(kstrAmazonUrlScheme) == true);
+			}
+		}
+		return false;
+	}
+	//--------------------------------------------------------
+	/// This intercepts url clicks within the webview and
+	/// ensures they are not opened in an external browser. 
+	/// It will also check if the link was a store deep link 
+	/// and instead  open the store.
+	///
+	/// @author S Hendrie
+	///
+	/// @param The URL string.
+	///
+	/// @return Whether or not it is a store deep link.
+	//--------------------------------------------------------
+	@Override public boolean shouldOverrideUrlLoading(WebView inView, String instrUrl)
+	{
+		Activity activity = CSApplication.get().getActivity();
+		if (activity != null && IsStoreDeepLink(instrUrl) == true)
+		{
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.setData(Uri.parse(instrUrl));
+			activity.startActivity(intent);
+		}
+		else
+		{
+			inView.loadUrl(instrUrl); 
+		}
+		
 	    return true;
 	}	
 	
