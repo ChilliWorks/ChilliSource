95e8e9e406b2ba8aefea9aa4ba74d81fc87cd51a
SDownie <scott.downie@tag-games.com>
2014-04-10 17:01:13 +0100

    Refactoring shader interface to use new documentation and resource type

---
+++ Source/ChilliSource/Rendering/Shader/Shader.cpp
@@ -0,0 +1,17 @@
+//
+//  Shader.cpp
+//  Chilli Source
+//
+//  Created by Scott Downie on 22/11/2010.
+//  Copyright (c) 2010 Tag Games Ltd. All rights reserved.
+//
+
+#include <ChilliSource/Rendering/Shader/Shader.h>
+
+namespace ChilliSource
+{
+	namespace Rendering
+	{
+        CS_DEFINE_NAMEDTYPE(Shader);
+    }
+}
--- Source/ChilliSource/Rendering/Shader/Shader.h
+++ Source/ChilliSource/Rendering/Shader/Shader.h
@@ -1,43 +1,51 @@
-/*
- * File: Shader.h
- * Date: 22/11/2010 2010 
- * Description: Abstract interface to concrete shader container such as GLShader.
- */
+//
+//  Shader.h
+//  Chilli Source
+//
+//  Created by Scott Downie on 22/11/2010.
+//  Copyright (c) 2010 Tag Games Ltd. All rights reserved.
+//
 
-/*
- * Author: Scott Downie
- * Version: v 1.0
- * Copyright ©2010 Tag Games Limited - All rights reserved 
- */
-
-#ifndef _MO_FLO_RENDERING_SHADER_H_
-#define _MO_FLO_RENDERING_SHADER_H_
+#ifndef _CHILLISOURCE_RENDERING_SHADER_SHADER_H_
+#define _CHILLISOURCE_RENDERING_SHADER_SHADER_H_
 
 #include <ChilliSource/ChilliSource.h>
-#include <ChilliSource/Core/Resource/ResourceOld.h>
+#include <ChilliSource/Core/Resource/Resource.h>
 
 namespace ChilliSource
 {
 	namespace Rendering
 	{
-		enum class ShaderType
-		{
-            k_combined,
-            k_vertex,
-            k_fragment
-		};
-		
-		class Shader : public Core::ResourceOld
+        //------------------------------------------------
+        /// Interface into a shader resource. Shaders
+        /// are rendering platform dependent and are used
+        /// by the renderer to decide how to pass data
+        /// to the currently executing shader program
+        ///
+        /// @author S Downie
+        //------------------------------------------------
+		class Shader : public Core::Resource
 		{
 		public:
 			
+            CS_DECLARE_NAMEDTYPE(Shader);
+            
+            //------------------------------------------------
+            /// Virtual destructor
+            ///
+            /// @author S Downie
+            //------------------------------------------------
 			virtual ~Shader(){}
-			CS_DECLARE_NAMEDTYPE(Shader);
 			
 		protected:
 			
-			//Can only be created via the resource manager
-			Shader(){}
+            //------------------------------------------------
+            /// Private constructor to ensure that
+            /// "abstract" shader cannot be created
+            ///
+            /// @author S Downie
+            //------------------------------------------------
+			Shader() = default;
 		};
 	}
 }
