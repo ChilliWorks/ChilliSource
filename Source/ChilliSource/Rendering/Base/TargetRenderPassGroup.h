1fa6511f05516a81bae2ca7a650251710c523ba8
Hugh McLaughlin <hugh.mclaughlin@tag-games.com>
2016-05-30 14:06:10 +0100

    Fixed date

--- Source/ChilliSource/Rendering/Base/TargetRenderPassGroup.h
+++ Source/ChilliSource/Rendering/Base/TargetRenderPassGroup.h
@@ -1,7 +1,7 @@
 //
 //  The MIT License (MIT)
 //
-//  Copyright (c) 2010 Tag Games Limited
+//  Copyright (c) 2016 Tag Games Limited
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
