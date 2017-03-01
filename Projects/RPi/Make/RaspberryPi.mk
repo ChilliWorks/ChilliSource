d72a05db983f579100dc80e5071643ee017aa41e
Jordan Brown <jordan.brown@tag-games.com>
2017-03-01 15:51:53 +0000

    Started RPi makefile stuff.

---
+++ Projects/RPi/Make/RaspberryPi.mk
@@ -0,0 +1,45 @@
+#
+#  RaspberryPi.mk
+#  ChilliSource
+#  Created by Jordan Brown on 01/03/2017
+#
+#  The MIT License (MIT)
+#
+#  Copyright (c) 2017 Tag Games Limited
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+#
+
+#prepare the ChilliSource libraries
+CS_PROJECT_ROOT=../../../../../../..
+include $(CS_PROJECT_ROOT)/ChilliSource/Tools/Scripts/RasPiBuildConfig.mk
+
+#gather all files in the engine that should be built
+CS_SOURCEFILES_CHILLISOURCE := $(shell 'python' '$(CS_SCRIPT_GETFILESWITHEXTENSIONS)' '--directory' '$(CS_PROJECT_ROOT)/ChilliSource/Source/ChilliSource/' '--extensions' 'cpp,c,cc')
+CS_SOURCEFILES_PLATFORM := $(shell 'python' '$(CS_SCRIPT_GETFILESWITHEXTENSIONS)' '--directory' '$(CS_PROJECT_ROOT)/ChilliSource/Source/CSBackend/Platform/RaspberryPi/' '--extensions' 'cpp,c,cc')
+CS_SOURCEFILES_RENDERING := $(shell 'python' '$(CS_SCRIPT_GETFILESWITHEXTENSIONS)' '--directory' '$(CS_PROJECT_ROOT)/ChilliSource/Source/CSBackend/Rendering/OpenGL/' '--extensions' 'cpp,c,cc')
+
+#build the ChilliSource static library
+include $(CLEAR_VARS)
+LOCAL_MODULE := ChilliSource
+LOCAL_CXXFLAGS := $(CS_CXXFLAGS)
+LOCAL_SRC_FILES := $(CS_SOURCEFILES_CHILLISOURCE) $(CS_SOURCEFILES_PLATFORM) $(CS_SOURCEFILES_RENDERING)
+LOCAL_C_INCLUDES := $(CS_C_INCLUDES)
+LOCAL_SHORT_COMMANDS := true
+include $(BUILD_STATIC_LIBRARY)
---
+++ Tools/Scripts/RasPiBuildInitialise.mk
@@ -0,0 +1,57 @@
+#
+#  RasPiBuildInitialise.mk
+#  ChilliSource
+#  Created by Jordan Brown on 01/03/2017.
+#
+#  The MIT License (MIT)
+#
+#  Copyright (c) 2017 Tag Games Limited
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+#
+
+#------------------------------------------------------------------------------
+# This copies the required static libraries to the currently built project. 
+# The android build settings are also included, so this is the only thing that
+# a makefile building a ChilliSource shared library will need to include. 
+# 
+# Prior to including this, CS_PROJECT_ROOT must be set to the relative path to
+# the root of the project, i.e, the directory ChilliSource is located in.
+#
+# @author Jordan Brown
+#------------------------------------------------------------------------------
+
+include $(call my-dir)/RasPiBuildConfig.mk
+
+ifndef CS_CLEAN
+
+#copy pre-built static libraries
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(CS_MODULENAME_CSBASE)
+LOCAL_SRC_FILES := $(CS_PROJECT_ROOT)/ChilliSource/Libraries/Core/RPi/Libs/libCSBase.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(CS_MODULENAME_CHILLISOURCE)
+#LOCAL_SRC_FILES := $(CS_PROJECT_ROOT)/ChilliSource/Projects/RPi/chillisource/libs/$(TARGET_ARCH_ABI)/libChilliSource.a
+LOCAL_SRC_FILES := $(CS_PROJECT_ROOT)/ChilliSource/Projects/RPi/libChilliSource.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+endif
+
