eb4643e844c0ec5fb37b15fe251d5d8a34e5bbe8
Scott Downie <scott.downie@tag-games.com>
2017-04-04 09:25:47 +0100

    Fixing bug on Windows backend where minimising the window caused GL crash

--- Source/CSBackend/Platform/Windows/SFML/Base/SFMLWindow.cpp
+++ Source/CSBackend/Platform/Windows/SFML/Base/SFMLWindow.cpp
@@ -599,9 +599,12 @@ namespace CSBackend
 					}
 				}
 
-                m_lifecycleManager->SystemUpdate();
-                m_lifecycleManager->Render();
-                m_window.display();
+				if (m_isFocused == true)
+				{
+                	m_lifecycleManager->SystemUpdate();
+                	m_lifecycleManager->Render();
+                	m_window.display();
+				}
 
 				if (m_quitScheduled)
 				{
